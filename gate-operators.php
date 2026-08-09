624751ca1fe6074cc01ab3e17a6d3e545dc0fb3a
Zulkifal <zulkifal1976@gmail.com>
2026-08-09 21:13:31 +0500

    Add gate operators
    Demonstrate usage of logical operators in PHP with examples.

---
+++ gate-operators.php
@@ -0,0 +1,44 @@
+<?php
+      $galaxy_9095 = 20;
+       $galaxy_9096 = 60;
+         if ( $galaxy_9095 == 20 and $galaxy_9096 == 60 ) {
+             echo 'i love you!';// write I love you when both condition is true
+             echo "<br>";
+         }
+           //OR operator
+           $galaxy_10395 = 50;
+             $galaxy_10396 = 60;
+              if ($galaxy_10395 == 50 or $galaxy_10396 == 50) {
+                  echo 'i love you! 2X';// write I love u when one condition is true
+                  echo "<br>";
+              }
+          //XOR operator
+           $galaxy_1255 = 60;
+              $galaxy_1256 = 40;
+                if ( $galaxy_1255 == 80 xor $galaxy_1256 == 40 ) {
+                    echo 'i love you! 3X';//if one condition is correct it will show true if both are correct it will show false.
+                    echo "<br>";
+                }
+                    //now and operator
+                  $galaxy_1258 = 10;
+                    $galaxy_1259 = 20;
+                      if ( $galaxy_1258 == 10 and $galaxy_1259 == 20 ) {
+                          echo 'i love you! 4X'; // show true if both are correct
+                          echo "<br>";
+                      }
+                          //OR operator
+                        $galaxy_2288 = 10;
+                          $galaxy_2289 = 20;
+                           if ($galaxy_2288 == 10 || $galaxy_2289 == 90) {
+                               echo 'i love you! 5X';//one of them  is true
+                               echo "<br>";
+                           }
+                                //  not operator
+                                    $galaxy_332211 = 10;
+                                         if (!($galaxy_332211 == 20) ) {
+                                             echo 'i love you! 6X';// if the variable is wrong it will show true and vice verse
+                                             echo "<br>";
+                                         }
+//Code source form galaxy. Open Source
+//code password F392O9B5SK3KSS00LXA
+//copyright without developer permission is prohibited
